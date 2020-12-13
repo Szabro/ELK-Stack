1cc2cb3867fbb3f1b0ed8bd2f1cda8fc2e5aca05
Christopher Szabo <christopherszabo@ChristozabosMBP.attlocal.net>
2020-12-12 20:47:51 -0800

    First commit

---
+++ Linux/machine info.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir ~/research 2> /dev/null
+
+echo "A Quick System Audit Script"
+date
+echo ""
+echo "Machine Type Info:"
+echo $MACHTYPE
+echo -e "Uname info: $(uname -a) \n"
+echo -e "IP Info: $(ip addr | grep inet | tail -2 | head -1) \n"
+echo "Hostname: $(hostname -s) "
+echo -e "\n777 Files:" >>  ~/research/sys_info.txt
+find / -type f -perm 777 >> ~/research/sys_info.txt
+echo -e "\nTop 10 Processes" >> ~/research/sys_info.txt
+ps aux -m | awk {'print $1, $2, $3, $4, $11'} | head >> ~/research/sys_info.txt
